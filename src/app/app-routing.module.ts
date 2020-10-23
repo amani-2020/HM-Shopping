7707b97015400e97baf6167a81c97f88cefe1556
amani <amaniheritage@gmail.com>
2020-10-23 18:44:35 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { LoginComponent } from './login/login.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'products', component: ProductsComponent},
+  {path: 'orders', component: MyOrdersComponent},
+  {path: 'admin/products', component: AdminProductsComponent},
+  {path: 'admin/orders', component: AdminOrdersComponent},
+  {path: 'shoppingcart', component: ShoppingCartComponent},
+  {path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
